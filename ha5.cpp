c15cba0b8bbca615b6c3008960cee273597cfaf0
Chandrahasa06 <142317595+Chandrahasa06@users.noreply.github.com>
2023-08-17 18:59:09 +0530

    Update ha5.cpp

--- ha5.cpp
+++ ha5.cpp
@@ -25,15 +25,34 @@ f=f*num;
 }
 return f;
 }
+double cosine(double x)
+{
+    double result = 0.0;
+    for (int n = 0; n < 10; ++n) 
+    {
+        result += pow(-1, n) * pow(x, 2 * n) / fact(2 * n);
+    }
+    return result;
+}
+double exponent(double x)
+{
+    double result=0.0;
+    for (int n=0;n<10;n++)
+    {
+        result+= pow(x,n) / fact(n);
+    
+    }
+    return result;
+}
 
 int main(){
 char choice;
 char trig;
-int angle;
-int rad;
+double angle;
+double rad;
 char ch;
 int n,i,a,b,basen,pown,num;
-cout<< "Type of operation(Algebraic- A/Trignometric-T/Factorial-F)";
+cout<< "Type of operation(Algebraic- A/Trignometric-T/Factorial-F/Exponent-E)";
 cin>> choice;
 if (choice == 'A' || choice == 'a'){
 cout << "enter the operator(+,-,*,/,^)";
@@ -60,32 +79,60 @@ cout << "Product of the numbers is: " << a*b << endl;
 break;
 case '/':
 cout << "Division of the numbers is: " << a/b <<endl;
+if(b==0)
+{
+    cout<<"Error!Division by zero";
+}
 break;
 case '^':
 cout<<"Base to the power is: " ;
-cout << exp(basen,pown);}
+cout << pow(basen,pown);
+break;
 
+
+default:
+cout<<"Invalid operator";
 }
-else if(choice == 'F' || choice == 'f'){
-cout << "Enter the number of which you want factorial:";
-cin>> num;
-cout<<"The factorial of the given number is";
-cout<< fact(num);}
+}
+else if(choice=='E' || choice == 'e')
+{
+    double num;
+    cout<<"Enter the exponent:";
+    cin>>num;
+    cout<<exponent(num);
+}
+else if(choice == 'F' || choice == 'f')
+{
+    cout << "Enter the number of which you want factorial:";
+    cin>> num;
+    cout<<"The factorial of the given number is";
+    cout<< fact(num);
+}
 else if(choice == 'T' || choice == 't'){
-cout<< "select the trignometric function(sin/cos/tan): ";
+cout<< "select the trignometric function(sin-s/cos-c): ";
 cin>> trig;
 cout<< "Enter the angle in degrees: ";
 cin >> angle;
-rad=(3*angle)/180;}
+rad=(3.14*angle)/180;}
+double x;
+  x=rad;
 if (trig == 's'){
-for(i=1;i<50;i+=2){
+for(i=3;i<15;i+=2){
 if (i%4==1){
-rad=(exp(rad,i)/fact(i))+rad;
+rad=(pow(x,i)/fact(i))+rad;
 }
-else{
-rad=rad-(exp(rad,i)/fact(i));
-}}
-cout<< rad;}
+else
+{
+    rad=rad-(pow(x,i)/fact(i));
+}
+}
+cout<< rad;
+}
+else if(trig=='c')
+{
+    cout<<cosine(rad)<<endl;
+}
+
 
 return 0;
 }
